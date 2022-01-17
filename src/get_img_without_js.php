97a35f4847bd13ade57ccbf77e881d7943daa277
novojilov_pavel <novojilov_pavel>
2022-01-18 02:39:01 +0500

    +

--- src/get_img_without_js.php
+++ src/get_img_without_js.php
@@ -2,15 +2,11 @@
 if (!function_exists('fb_cache')) {
     /** Пример получения картинки минимального размера/качества из кэша для первичного отображения
      * @param $img_web_url string  Относительно от публичной директории путь до изображения
-     * @param string|null $relative_path_for_cache [optional] Относительный путь от корневой директории сайта до кэша изображений, файлы в директории должны иметь публичный доступ для скачивания.
-     * @param string|null $public_work_path [optional] Публичный корневой каталог сайта.
-     * @param string|null $root_path [optional] Корневой каталог сайта.
      * @return string Возвращает относительный путь до изображения в кэше, но вернется пустая строка, если изображение не было сформировано в кэше
      * @throws exception
      */
-    function fb_cache(string $img_web_url, string $relative_path_for_cache = "/.fast_background", string $public_work_path = null, string $root_path = null): ?string
+    function fb_cache(string $img_web_url): ?string
     {
-        require_once 'fast_background.php';
         /** @var \showyweb\fast_background\fb $fast_background_obj */
         global $fast_background_obj;
         if (empty($fast_background_obj))
